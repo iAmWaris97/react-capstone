e163a6bf886c86bde8d5613b516a28c807d2e5fa
iAmWaris97 <haleem.waris@gmail.com>
2022-10-23 00:18:31 +0430

    Refactore coins component and remove bootstrap

--- src/components/Coins/coins.js
+++ src/components/Coins/coins.js
@@ -1,86 +1,66 @@
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import './Coins.css';
 import { useDispatch, useSelector } from 'react-redux';
-import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchAssests } from '../../redux/Coins/coinsSlice';
 
 const Coins = () => {
   const dispatch = useDispatch();
-  const assetList = useSelector((state) => state.coins);
+
+  const assets = useSelector((state) => state.coins);
   useEffect(() => {
-    if (!assetList.length) {
+    if (!assets.length) {
       dispatch(fetchAssests());
     }
   });
+
   const [searchcoin, setSearchcoin] = useState('');
   const onSearch = (e) => {
     setSearchcoin(e.target.value);
   };
-  const searched = assetList.filter(
+
+  const searched = assets.filter(
     (filteredCoin) => filteredCoin.name.toLowerCase().includes(searchcoin.toLowerCase())
       || filteredCoin.symbol.toLowerCase().includes(searchcoin.toLowerCase()),
   );
 
   return (
-    <>
-      <nav className="navbar">
-        <h1>SomeApp</h1>
-      </nav>
-      <Container>
-        <Row>
-          <Col className="welcome">
-            <div className="welcome-body">
-              <p>Welcome!</p>
-              <p>CryptoHouse</p>
-            </div>
-          </Col>
-        </Row>
-        <Row className="filter">
-          <div className="filter-body">
-            <span>Filter:</span>
-            <input
-              type="text"
-              size="sm"
-              value={searchcoin}
-              onChange={onSearch}
-            />
-          </div>
-        </Row>
-        <Row className="cryptos">
-          {searched.map((asset) => (
-            <Col
-              xs={6}
-              className="coll"
-              Key={`${asset.asset_id}${Math.random * 10}`}
+    <div className="container">
+      <div className="search">
+        <input
+          type="text"
+          value={searchcoin}
+          onChange={onSearch}
+        />
+      </div>
+      <div className="coins">
+        {searched.map((asset) => (
+          <div
+            className="coll"
+            key={`${asset.asset_id}${Math.random * 10}`}
+          >
+            <Link
+              className="coin-card"
+              Key={asset.asset_id}
+              to={`/coin/${asset.id}`}
             >
-              <Link
-                className="crypto-card"
-                Key={asset.asset_id}
-                to={`/crypto/${asset.id}`}
-              >
-                <Row className="asset-price">
-                  <p>{asset.price}</p>
-                </Row>
-                <Row>
-                  <Col>
-                    <Image className="icon" src={asset.icon} />
-                  </Col>
-                  <Col className="asset-detail">
-                    <span className="asset-code">{asset.symbol}</span>
-                    <span className="asset-name">{asset.name}</span>
-                  </Col>
-                </Row>
-                <Row />
-              </Link>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </>
+              <div className="price">
+                <p>{asset.price}</p>
+              </div>
+              <div>
+                <div>
+                  <img className="icon" src={asset.icon} alt={asset.name} />
+                </div>
+                <div className="asset-detail">
+                  <span className="asset-code">{asset.symbol}</span>
+                  <span className="asset-name">{asset.name}</span>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
